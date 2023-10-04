9279a7ee4651012d014e6f123e974141ee64c68b
Juan-Fernando <juprmora@ucsc.edu>
2023-10-03 18:07:58 -0700

    DFS with visited notes

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -1,7 +1,7 @@
 """
 In this file, you will implement generic search algorithms which are called by Pacman agents.
 """
-
+from pacai.core.directions import Directions
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first [p 85].
@@ -11,16 +11,41 @@ def depthFirstSearch(problem):
 
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
-    ```
-    print("Start: %s" % (str(problem.startingState())))
-    print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
-    print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
-    ```
+    
+ 
     """
-
+    w = Directions.WEST
+    dirs = {'North': Directions.NORTH, 
+            'East': Directions.EAST, 
+            'South': Directions.SOUTH,
+            'West': Directions.WEST} 
     # *** Your Code Here ***
-    raise NotImplementedError()
+    path = problem.successorStates(problem.startingState())
+    #while goal not found and stack not empty
 
+    visited = [problem.startingState()]
+    print(visited)
+    
+    while len(path) > 0:
+        node = path.pop()
+        visited.append(node[0])
+       
+        if problem.isGoal(node[0]):
+            print("found")
+            break
+        for successor in problem.successorStates(node[0]):
+            if not (successor[0] in visited):
+                path.append(successor)
+     
+    return [dirs[tup[1]] for tup in path]
+
+    #print([successor for successor in problem.successorStates(problem.startingState())])
+    #print("Start:", problem.startingState())
+    #print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
+    #print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
+       
+    #raise NotImplementedError()
+    return [] 
 def breadthFirstSearch(problem):
     """
     Search the shallowest nodes in the search tree first. [p 81]
